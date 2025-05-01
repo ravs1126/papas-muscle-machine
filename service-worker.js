25113bee111fae699d917979b7509853428c8412
Ravi J <ravi_jaglal@hotmail.com>
2025-05-01 03:13:08 -0400

    edited the service chap again

--- service-worker.js
+++ service-worker.js
@@ -14,12 +14,12 @@ self.addEventListener('install', evt =>
 
 self.addEventListener('activate', evt =>
   evt.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-    .then(() => self.clients.claim())  // Claim control of all clients immediately
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())   // take control right away
   )
 );
 
